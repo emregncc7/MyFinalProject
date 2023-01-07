586575dd69965d2b41e742a2fe6f074dad97f14a
emre genc <emregenc696@outlook.com>
2023-01-07 13:00:46 +0300

    EfCoreProject

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entites.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Generic constraint
+    //class: referans tip
+   public interface IEntityRepository<T> 
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
---
+++ Entites/Concrete/Customer.cs
@@ -0,0 +1,15 @@
+﻿using Entites.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entites.Concrete
+{
+   public class Customer : IEntity
+    {
+        public string CustomerId { get; set; }
+        public string ContackName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+    }
+}
